d66d124b3a2f42aca2bf900410b0ab7ca2172d01
Derek Jacobi <derekjacobi@gmail.com>
2015-07-16 15:43:16 -0400

    maps added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,3 @@
 //= require best_in_place
 //= require materialize-sprockets
 //= require_tree .
-
-<script src="https://maps.googleapis.com/maps/api/js"></script>
-  <script>
-    function initialize() {
-      var mapCanvas = document.getElementById('map-canvas');
-      var mapOptions = {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      var map = new google.maps.Map(mapCanvas, mapOptions)
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
-  </script>
