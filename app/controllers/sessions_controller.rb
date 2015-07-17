3496330361dc6d5ae660da3734579e7aed007d00
Derek Jacobi <derekjacobi@gmail.com>
2015-07-17 11:34:30 -0400

    fixed incorrect/correct logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: user_params[:email])
 
-    if @user.authenticate(user_params[:password])
+    if @user && @user.authenticate(user_params[:password])
       login!(@user)
       redirect_to @user
     else
