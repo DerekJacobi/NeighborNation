b2bf44a25b9d20cb3e62e39c4ea8fc3128fa41f5
Derek Jacobi <derekjacobi@gmail.com>
2015-07-23 11:01:00 -0400

    merge con with schema version number

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20150722190638) do
+ActiveRecord::Schema.define(version: 20150722212546) do
 
 
   # These are extensions that must be enabled in order to support this database
