33d3d3aa36199e8e6044dd4a356b6cddcc0fd32e
Derek Jacobi <derekjacobi@gmail.com>
2015-07-24 10:09:25 -0400

    aws keys updated for protectionw

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,6 @@
 
 AWS.config(
    :access_key_id => ENV['ACCESS_KEY_ID'],
-   :secret_access_key => ENV['SECRET_ACCESS_KEY']
+   :secret_access_key => ENV['SECRET_ACCESS_KEY'],
+   :S3_BUCKET => ENV['S3_BUCKET_NAME']
 )
