0077c9c11ddeacb36b4f3e3ae9895d6eb1142fb0
Derek Jacobi <derekjacobi@gmail.com>
2015-07-24 09:42:19 -0400

    aws keys updated for protectionw

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,3 +1,3 @@
 AWS.config(
-access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
